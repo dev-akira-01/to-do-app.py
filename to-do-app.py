76e729594b891f3cb2d2e638865797c3bce22bfc
Akira <akira.dev.tech@gmail.com>
2026-03-31 19:20:21 +0530

    add remove_task function

--- to-do-app.py
+++ to-do-app.py
@@ -23,6 +23,21 @@ def view_task():
         status = "✓" if task["done"] else "✕"
         print(f"{index}. {task['tasks']} [{status}]")
 
+def remove_task():
+    if not tasks:
+        print("There are no tasks added yet!")
+        return
+    try:
+        index_del = int(input("Enter the task number to be deleted: ")) - 1
+        if 0 < index_del <= len(tasks):
+            tasks.pop(index_del)
+        else: 
+            print("Number out of range")
+    except: 
+        print("Please input a valid number")
+
+
+
 
 while True:
     menu()
@@ -32,6 +47,8 @@ while True:
             add_task()
         elif choice == "4":
             view_task()
+        elif choice == "2":
+            remove_task()
         else: 
             print("Please enter a valid number")
     except:
