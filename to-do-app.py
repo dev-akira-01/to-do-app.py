09cce4039c60a837453eae97869637cd124414bf
Akira <akira.dev.tech@gmail.com>
2026-03-31 16:34:14 +0530

    add todo.py , created function of adding tasks

--- to-do-app.py
+++ to-do-app.py
@@ -1,1 +1,12 @@
 #initiate
+
+tasks= [] #empty list
+
+def task_list():
+    
+
+
+def add_task():
+    task= input("Enter task to be added: ")
+    tasks.append({"tasks":task,"done":False})
+    print(f"'{task}' was added successfully!")
